a98c62ee98cdc349381a5f70e4faf76810c515e2
yoobin <yoobin9255@gmail.com>
2023-09-21 12:12:45 +0900

    <feature> create basic page component

---
+++ src/pages/QuestionResultPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function QuestionResultPage() {
+  return <div>QuestionResultPage</div>;
+}
+
+export default QuestionResultPage;
---
+++ src/pages/WrongQuestionsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WrongQuestionsPage() {
+  return <div>WrongQuestionsPage</div>;
+}
+
+export default WrongQuestionsPage;
