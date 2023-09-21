b07e373e7e9eb7200b8ac96c7ce72522d6f0c92d
yoobin <yoobin9255@gmail.com>
2023-09-22 08:52:50 +0900

    <test> add reset store logic on customRender

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,8 +1,32 @@
 import { render, type RenderOptions } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import createSagaMiddleware from 'redux-saga';
+import questionsSliceReducer from './redux/questions/reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import watch from './redux/sagas';
 
 function WrapperComponent({ children }: { children: React.ReactNode }) {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = configureStore({
+    reducer: { questions: questionsSliceReducer },
+    middleware: (getDefaultMiddleware) => [
+      sagaMiddleware,
+      ...getDefaultMiddleware(),
+    ],
+    preloadedState: {
+      questions: {
+        isLoading: false,
+        questions: [],
+        correctAnsweredQuestions: [],
+        wrongAnsweredQuestions: [],
+        error: null,
+      },
+    },
+  });
+
+  sagaMiddleware.run(watch);
+
   return <Provider store={store}>{children}</Provider>;
 }
 
