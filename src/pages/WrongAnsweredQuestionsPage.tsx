e5a5418e1ffe4937e61035e28a51156d3a1094ce
yoobin <yoobin9255@gmail.com>
2023-09-22 17:55:07 +0900

    <feature> create WrongAnsweredQuestionPage based on test passing

--- src/pages/WrongAnsweredQuestionsPage.tsx
+++ src/pages/WrongAnsweredQuestionsPage.tsx
@@ -6,7 +6,41 @@ export type WrongAnsweredQuestionType = {
 };
 
 function WrongAnsweredQuestionsPage() {
-  return <div>WrongAnsweredQuestionsPage</div>;
+  const wrongAnsweredQuestionData = localStorage.getItem(
+    'wrong-answered-questions'
+  );
+
+  const wrongAnsweredQuestionHistory: WrongAnsweredQuestionType[] =
+    wrongAnsweredQuestionData ? JSON.parse(wrongAnsweredQuestionData) : [];
+
+  if (wrongAnsweredQuestionHistory.length <= 0) {
+    return (
+      <>
+        <h1>오답 노트</h1>
+        <h3>오답 기록이 없습니다.</h3>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>오답 노트</h1>
+      {wrongAnsweredQuestionHistory.map((history) => {
+        return (
+          <div key={history.question}>
+            <h3>문제: {history.question}</h3>
+            <div>선택한 오답: {history.chosenAnswer}</div>
+            <div>정답: {history.correctAnswer}</div>
+            <ul>
+              {history.answers.map((answer) => (
+                <li key={answer}>{answer}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default WrongAnsweredQuestionsPage;
