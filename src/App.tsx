6bd3f64dc2f4795536492c0ed1c5471a566c9503
yoobin <yoobin9255@gmail.com>
2023-09-22 19:07:31 +0900

    <feature> add navbar following by solving  Navbar test

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ import MainPage from './pages/MainPage';
 import QuestionPage from './pages/QuestionPage';
 import QuestionsResultPage from './pages/QuestionsResultPage';
 import WrongAnsweredQuestionsPage from './pages/WrongAnsweredQuestionsPage';
+import Layout from './components/Layout';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const { pathname } = useLocation();
+  const layoutType = pathname === '/' ? 'main' : 'default';
+
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/questions" element={<QuestionPage />} />
-      <Route path="/questions-result" element={<QuestionsResultPage />} />
-      <Route
-        path="/wrong-answered-questions"
-        element={<WrongAnsweredQuestionsPage />}
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Layout layoutType={layoutType}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/questions" element={<QuestionPage />} />
+        <Route path="/questions-result" element={<QuestionsResultPage />} />
+        <Route
+          path="/wrong-answered-questions"
+          element={<WrongAnsweredQuestionsPage />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+function NavBar() {
+  return (
+    <NavBarContainer>
+      <Link to="/">홈</Link>
+      <Link to="/wrong-answered-questions">오답 노트</Link>
+    </NavBarContainer>
+  );
+}
+
+const NavBarContainer = styled.nav`
+  display: flex;
+  gap: 20px;
+`;
+
+export default NavBar;
