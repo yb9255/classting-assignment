8e4c3c73ea545ee34695e94d951adbde7368f092
yoobin <yoobin9255@gmail.com>
2023-09-23 21:22:37 +0900

    <test> add test case for 돌아가기 button and resetting the result logic

--- src/pages/tests/QuestionResultPage.test.tsx
+++ src/pages/tests/QuestionResultPage.test.tsx
@@ -263,4 +263,189 @@ describe('QuestionResult', () => {
     const mainTitle = screen.getByRole('heading', { name: '영화 퀴즈' });
     expect(mainTitle).toBeInTheDocument();
   });
+
+  it('returns home when click 돌아가기 button', async () => {
+    render(
+      <MemoryRouter initialEntries={['/questions-result']}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/questions-result" element={<QuestionsResultPage />} />
+        </Routes>
+      </MemoryRouter>,
+      {
+        preloadedState: {
+          questions: {
+            startTime: 0,
+            endTime: 650000,
+          },
+        },
+      }
+    );
+
+    const backButton = screen.getByRole('link', { name: '돌아가기' });
+    expect(backButton).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(backButton);
+    });
+
+    const mainTitle = await screen.findByRole('heading', { name: '영화 퀴즈' });
+    expect(mainTitle).toBeInTheDocument();
+  });
+
+  it('reset the result when it is unmounted', async () => {
+    render(
+      <>
+        <div id="overlay-root" />
+        <MemoryRouter initialEntries={['/questions']}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/questions" element={<QuestionPage />} />
+            <Route path="/questions-result" element={<QuestionsResultPage />} />
+          </Routes>
+        </MemoryRouter>
+      </>
+    );
+
+    const correctAnswer = await screen.findByText(
+      decodeHtmlString('The Lead Programmer&#039;s birthday')
+    );
+
+    await waitFor(async () => {
+      await user.click(correctAnswer);
+    });
+
+    const messageModal = await screen.findByText('정답입니다!', {
+      exact: false,
+    });
+
+    const nextBtn = await screen.findByRole('button', { name: '다음' });
+
+    expect(messageModal).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(nextBtn);
+    });
+
+    const nextQuestionTitle = await screen.findByText(
+      decodeHtmlString(
+        'Johnny Cash did a cover of this song written by lead singer of Nine Inch Nails, Trent Reznor.'
+      ),
+      { exact: false }
+    );
+
+    expect(nextQuestionTitle).toBeInTheDocument();
+
+    const nextQuestionWrongAnswer = await screen.findByText(
+      decodeHtmlString('Closer')
+    );
+
+    expect(nextQuestionWrongAnswer).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(nextQuestionWrongAnswer);
+    });
+
+    const nextQuestionMessageModal = await screen.findByText('틀렸습니다!', {
+      exact: false,
+    });
+
+    const nextQuestionNextBtn = await screen.findByRole('button', {
+      name: '다음',
+    });
+
+    expect(nextQuestionMessageModal).toBeInTheDocument();
+    expect(nextQuestionNextBtn).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(nextQuestionNextBtn);
+    });
+
+    const correctAnswerCountDiv = screen.getByText('정답 수:', {
+      exact: false,
+    });
+
+    const wrongAnswerCountDiv = screen.getByText('오답 수: ', {
+      exact: false,
+    });
+
+    expect(correctAnswerCountDiv).toHaveTextContent('1');
+    expect(wrongAnswerCountDiv).toHaveTextContent('1');
+
+    const backButton = screen.getByRole('link', { name: '돌아가기' });
+    expect(backButton).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(backButton);
+    });
+
+    const mainButton = screen.getByRole('link', { name: '시작하기' });
+
+    await waitFor(async () => {
+      await user.click(mainButton);
+    });
+
+    const secondTestCorrectAnswer = await screen.findByText(
+      decodeHtmlString('The Lead Programmer&#039;s birthday')
+    );
+
+    await waitFor(async () => {
+      await user.click(secondTestCorrectAnswer);
+    });
+
+    const secondTestMessageModal = await screen.findByText('정답입니다!', {
+      exact: false,
+    });
+
+    const secondTestNextBtn = await screen.findByRole('button', {
+      name: '다음',
+    });
+
+    expect(secondTestMessageModal).toBeInTheDocument();
+    expect(secondTestNextBtn).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(secondTestNextBtn);
+    });
+
+    const secondNextQuestionCorrectAnswer = await screen.findByText(
+      decodeHtmlString('Hurt')
+    );
+
+    expect(secondNextQuestionCorrectAnswer).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(secondNextQuestionCorrectAnswer);
+    });
+
+    const secondNextQuestionMessageModal = await screen.findByText(
+      '정답입니다!',
+      {
+        exact: false,
+      }
+    );
+
+    const secondNextQuestionNextBtn = await screen.findByRole('button', {
+      name: '다음',
+    });
+
+    expect(secondNextQuestionMessageModal).toBeInTheDocument();
+    expect(secondNextQuestionNextBtn).toBeInTheDocument();
+
+    await waitFor(async () => {
+      await user.click(secondNextQuestionNextBtn);
+    });
+
+    const secondTestCorrectAnswerCountDiv = screen.getByText('정답 수:', {
+      exact: false,
+    });
+
+    const secondTestWrongAnswerCountDiv = screen.getByText('오답 수: ', {
+      exact: false,
+    });
+
+    expect(secondTestCorrectAnswerCountDiv).toHaveTextContent('2');
+    expect(secondTestWrongAnswerCountDiv).toHaveTextContent('0');
+  });
 });
