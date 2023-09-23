66dec90e8b5bb0f055b70ca1954d0391b085e97e
yoobin <yoobin9255@gmail.com>
2023-09-23 19:46:53 +0900

    <style> add han-sans-korean font

--- src/GlobalStyleProvider.ts
+++ src/GlobalStyleProvider.ts
@@ -3,6 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyleProvider = createGlobalStyle`
   body {
     background-color: #fbebe4;
+    font-family: "source-han-sans-korean", sans-serif;
+    font-weight: 400;
+    font-style: normal;    
   }
 `;
 
