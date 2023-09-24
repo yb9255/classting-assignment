132cfcbf1c2cc11ee205f37fa3630d3566ea6223
yoobin <yoobin9255@gmail.com>
2023-09-24 18:08:48 +0900

    <fix> migrate from axios to built-in fetch

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -4,29 +4,29 @@ import {
   fetchQuestionsSuccess,
   fetchQuestionFailure,
 } from '../questions/reducer';
-import axios, { type AxiosResponse } from 'axios';
 import { OriginalQuestionType } from '../questions/types';
 import { DB_API_URL } from '../../constants';
 
-type ExpectedAxiosResponse = AxiosResponse<{
+type ResponseBody = {
   response_code: number;
   results: OriginalQuestionType[];
-}>;
+};
 
 export function* watchFetchQuestions() {
   const query = '?amount=10&category=11&type=multiple';
 
   try {
-    const questionsResponse: ExpectedAxiosResponse = yield call(() =>
-      axios.get(`${DB_API_URL}${query}`)
+    const questionsResponse: Response = yield call(() =>
+      fetch(`${DB_API_URL}${query}`)
     );
+    const questionResponseBody: ResponseBody = yield questionsResponse.json();
 
-    const isSuccess = questionsResponse.data.response_code <= 1;
+    const isSuccess = questionResponseBody.response_code <= 1;
 
     if (isSuccess) {
       yield put(
         fetchQuestionsSuccess({
-          results: questionsResponse.data.results,
+          results: questionResponseBody.results,
         })
       );
       return;
